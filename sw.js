09cbcea8fa9e674f3ab52ccdb963f60360a3a039
SForceDev123 <103314284+SForceDev123@users.noreply.github.com>
2026-02-24 22:35:18 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "quran-quest-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).then((net) => {
+      const copy = net.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return net;
+    }).catch(() => caches.match("./")))
+  );
+});
